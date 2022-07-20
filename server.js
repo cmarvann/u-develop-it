3194583deb64457fb1363bd70f547fc12de414ad
cmarvann <covingtonmarvann@gmail.com>
2022-07-20 11:49:18 -0400

    preview sever

--- server.js
+++ server.js
@@ -8,22 +8,17 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// get route
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
+  
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
   });
   
+
 // start server 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     });
 
-    // Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
-  
+
     
